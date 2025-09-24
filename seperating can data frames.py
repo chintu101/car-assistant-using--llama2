4feb926ac94fb80a49e7c322fad145775958a444
chintu101 <85573491+chintu101@users.noreply.github.com>
2025-09-24 18:51:21 +0530

    converting mdf to csvs then halving the data size

--- seperating can data frames.py
+++ seperating can data frames.py
@@ -1,2 +1,17 @@
 import csv
-import pandas
+import pandas as pd
+from asammdf import MDF
+
+# Load your MF4 file
+df = pd.read_csv("D:\DevTools\Projects\LLMS\data\Tesla_log.csv")
+half = len(df) // 2
+df = df.iloc[:half]
+
+# Export to CSV
+df.to_csv("D:\DevTools\Projects\LLMS\data\Tesla_log.csv")
+
+
+
+'''df = pd.read_csv("D:\DevTools\Projects\LLMS\data\CAN signal extraction and translation dataset\log 1.csv")
+print(df)'''
+
